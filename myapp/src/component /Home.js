32e1f7b4cd27151fe00d83e01a7522fd92bef683
Abhishek Patidar <107181809+mrabhee@users.noreply.github.com>
2024-08-20 11:02:59 +0000

    Responsive

--- myapp/src/component /Home.js
+++ myapp/src/component /Home.js
@@ -2,18 +2,18 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center md:space-x-10 p-8">
+    <div className="flex flex-col md:flex-row justify-center items-center h-dvh md:space-x-10 p-8">
       {/* Left Div */}
-      <div className="flex justify-center items-center mb-8 md:mb-0">
+      <div className="flex justify-center w-dvw md:w-full items-center mb-8 md:mb-0 ">
         <img
           src="./images/hero.jpg"
           alt="Profile"
-          className="rounded-full w-48 h-48 md:max-w-5xl md:max-h-full"
+          className="rounded-full w-96  md:max-w-5xl md:max-h-full"
         />
       </div>
 
       {/* Right Div */}
-      <div className="text-center md:text-left">
+      <div className="text-center w-dvw md:w-full md:text-left ">
         <h1 className="text-3xl md:text-4xl font-bold text-gray-800">
           Abhishek Patidar
         </h1>
